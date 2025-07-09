4bfcfe42c3f3bb02d4385c8eff55aedb543af912
pauytrh2 <pauytrh@gmail.com>
2025-07-09 12:04:56 +0300

    Add python and rust formatting support

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -733,8 +733,7 @@ require('lazy').setup({
       }
     end,
   },
-
-  { -- Autoformat
+  {
     'stevearc/conform.nvim',
     event = { 'BufWritePre' },
     cmd = { 'ConformInfo' },
@@ -751,9 +750,6 @@ require('lazy').setup({
     opts = {
       notify_on_error = false,
       format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
         local disable_filetypes = { c = true, cpp = true }
         if disable_filetypes[vim.bo[bufnr].filetype] then
           return nil
@@ -766,11 +762,8 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        python = { 'isort', 'black' },
+        rust = { 'rustfmt' },
       },
     },
   },
