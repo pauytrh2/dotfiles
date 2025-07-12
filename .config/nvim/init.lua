f65de9499e2946cdecdbded05aa3562335ae489d
pauytrh2 <pauytrh@gmail.com>
2025-07-12 14:11:59 +0300

    Add relative numbers

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -166,6 +166,9 @@ vim.o.scrolloff = 10
 -- See `:help 'confirm'`
 vim.o.confirm = true
 
+-- Relative numbers for jumping around
+vim.wo.relativenumber = true
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
