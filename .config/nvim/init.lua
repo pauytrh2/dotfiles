d68529b926a48326295aa12878d390cecb42b5e9
pauytrh2 <pauytrh@gmail.com>
2025-07-13 18:23:59 +0300

    I am stan and i was wrong

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -212,7 +212,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 -- [[ Install `lazy.nvim` plugin manager ]]
--- See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+--    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
