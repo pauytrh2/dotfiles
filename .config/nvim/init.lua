ced1ca580882dbf4408331e592771c2dec7ace24
pauytrh2 <pauytrh@gmail.com>
2025-07-13 20:55:15 +0300

    Ultra hardcore vim user

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -182,11 +182,21 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
--- TIP: Disable arrow keys in normal mode
+-- TIP: Disable arrow keys
 vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!!"<CR>')
 vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!!"<CR>')
 vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!!"<CR>')
 vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!!"<CR>')
+
+vim.keymap.set('i', '<left>', '<cmd>echo "Use h to move!!!"<CR>')
+vim.keymap.set('i', '<right>', '<cmd>echo "Use l to move!!!"<CR>')
+vim.keymap.set('i', '<up>', '<cmd>echo "Use k to move!!!"<CR>')
+vim.keymap.set('i', '<down>', '<cmd>echo "Use j to move!!!"<CR>')
+
+vim.keymap.set('v', '<left>', '<cmd>echo "Use h to move!!!"<CR>')
+vim.keymap.set('v', '<right>', '<cmd>echo "Use l to move!!!"<CR>')
+vim.keymap.set('v', '<up>', '<cmd>echo "Use k to move!!!"<CR>')
+vim.keymap.set('v', '<down>', '<cmd>echo "Use j to move!!!"<CR>')
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
@@ -1003,6 +1013,11 @@ require('lazy').setup({
     },
   },
 })
+
+vim.opt.expandtab = true -- Use spaces instead of tabs
+vim.opt.shiftwidth = 4 -- Number of spaces for each indentation level
+vim.opt.tabstop = 4 -- Number of spaces a tab counts for
+vim.opt.softtabstop = 4 -- Number of spaces when pressing tab
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
