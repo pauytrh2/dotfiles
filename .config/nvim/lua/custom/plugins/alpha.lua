3f8dd18a568091ec0d07206be2948fedcfc0016e
pauytrh2 <pauytrh@gmail.com>
2025-07-14 11:36:19 +0300

    Modify alpha.nvim config: make header custom ascii art

--- .config/nvim/lua/custom/plugins/alpha.lua
+++ .config/nvim/lua/custom/plugins/alpha.lua
@@ -2,6 +2,18 @@ return {
   'goolord/alpha-nvim',
   dependencies = { 'echasnovski/mini.icons' },
   config = function()
-    require('alpha').setup(require('alpha.themes.startify').config)
+    local alpha = require 'alpha'
+    local startify = require 'alpha.themes.startify'
+
+    startify.section.header.val = {
+      ' __________                    ____   _____         ',
+      '\\______   \\____   __ ______ __\\   \\ /  /__| _____  ',
+      ' |     ___/__  \\ |  |  \\   |  |\\   \\  /|  |/     \\ ',
+      ' |    |    / __ \\_  |  /\\___  | \\    / |  |  | |  \\',
+      ' |____|   (____  /____/ / ____|  \\  /  |__|__|_|  /',
+      '               \\/       \\/        \\/            \\/ ',
+    }
+
+    alpha.setup(startify.config)
   end,
 }
