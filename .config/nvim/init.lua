733cccf3eb168708e8a6bc6f94766047082bd145
pauytrh2 <pauytrh@gmail.com>
2025-07-15 09:56:14 +0300

    Add pyright and rust_analyzer LSP

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -724,8 +724,19 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
+        pyright = {},
+        rust_analyzer = {
+          settings = {
+            ['rust-analyzer'] = {
+              cargo = {
+                allFeatures = true,
+              },
+              checkOnSave = {
+                command = 'clippy',
+              },
+            },
+          },
+        },
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
