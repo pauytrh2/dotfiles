766abe2fbd94f831886feb920900e7aa814b3f2a
pauytrh2 <pauytrh@gmail.com>
2025-07-20 12:52:35 +0300

    Add rust to ensure_installed

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1033,7 +1033,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'python', 'nasm' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'python', 'nasm', 'rust' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
